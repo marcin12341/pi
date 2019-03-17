360f636a102b7b0d8352045a078bdd2e0476c427
marcin <marcin12341@gmail.com>
2019-03-17 23:28:57 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+double get_random(int min, int max) {
+  return (max - min) * ((double)rand() / (double)RAND_MAX) + min;
+}
+
+double get_pi() {
+  double number;
+  const int SQUARE_POINTS = 10000;
+  int circle_points = 0;
+  time_t tt;
+  number = time(&tt);
+  srand(number);
+
+  for (int i = 0; i < SQUARE_POINTS; i++) {
+    double x = get_random(0, 1);
+    double y = get_random(0, 1);
+    if (x * x + y * y <= 1) {
+      circle_points++;
+    }
+  }
+  return 4 * (circle_points / (double)SQUARE_POINTS);
+}
+
+int main() {
+  int n = 5;
+  pid_t pid;
+  for (int i = 0; i < n; i++) {
+    pid = fork();
+    if (pid == 0) {
+      sleep(1);
+      break;
+    }
+  }
+  if (pid == 0) {
+    sleep(1);
+    double pi = get_pi();
+    printf("pi: %f\n", pi);
+  }
+}
